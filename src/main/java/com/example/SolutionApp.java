a8c2ed867573c93ac244b2f9276c6a2944ccd4d4
taras.chmeruk <taras.chmeruk@gameloft.com>
2024-03-21 11:26:43 +0200

    u

--- src/main/java/com/example/SolutionApp.java
+++ src/main/java/com/example/SolutionApp.java
@@ -1,87 +1,99 @@
 package com.example;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SolutionApp {
+    public static final String INPUT_TXT = "input.txt";
+    public static final String OUTPUT_TXT = "output.txt";
+    private static String abString;
+    private static final List<String> resultsList = new ArrayList<> ();
 
-    public static void main (String[] args) throws Exception {
+    public static void main (String[] args) throws IOException {
         readFile ();
     }
 
-    private static void readFile () throws Exception {
-        String fileName = "input.txt";
-        List<String> list;
-        try (BufferedReader reader = new BufferedReader (new FileReader (fileName))) {
-            list = reader.lines ().toList ();
-        }
-        processFile (list);
-    }
+    private static void readFile () throws IOException {
 
-    private static void processFile (final List<String> list) throws Exception {
-        int numberOfLines = Integer.parseInt (list.get (0).split (" ")[1]) + 2;
-        String s = list.get (1).trim ();
-        List<String> resultList = new ArrayList<> ();
-        for (int i = 2; i < numberOfLines; i++) {
-            String[] split = list.get (i).split (" ");
-            int l = Integer.parseInt (split[0]);
-            int r = Integer.parseInt (split[1]);
-            int k = Integer.parseInt (split[2]);
-            if (k > s.length ()) {
-                resultList.add ("-1");
-            } else {
-                int result = calculateOutputNumber (l, r, k, s.substring (l - 1, r));
-                resultList.add (String.valueOf (result));
+        try (BufferedReader reader = Files.newBufferedReader (Paths.get (INPUT_TXT))) {
+            String query;
+            int lineNumber = 0;
+            while ((query = reader.readLine ()) != null) {
+                if (lineNumber == 1) {
+                    abString = query;
+                } else if (lineNumber > 1) {
+                    int res = processLine (query);
+                    resultsList.add (String.valueOf (res));
+                }
+                lineNumber++;
             }
         }
-        writeFile (resultList);
+        writeFile ();
     }
 
-    private static void writeFile (List<String> result) throws Exception {
-        String fileName = "output.txt";
-        try (BufferedWriter bw = new BufferedWriter (new FileWriter (fileName))) {
-            for (int i = 0; i < result.size (); i++) {
-                bw.write (result.get (i));
-                if (i != result.size () - 1) {
+    private static int processLine (String line) {
+
+        String[] split = line.split ("\\s");
+        int l = Integer.parseInt (split[0]);
+        int r = Integer.parseInt (split[1]);
+        int k = Integer.parseInt (split[2]);
+        return calculateOutputNumber (l, r, k, abString.substring (l - 1, r));
+    }
+
+    public static int calculateOutputNumber (int l, int r, int k, String s) {
+        if (k > s.length ()) {
+            return -1;
+        }
+
+        int nonKCharCount = 0;
+        int kCharCount = 0;
+        char kChar = s.charAt (k - 1);
+        for (int i = 0; i < k; i++) {
+            if (s.charAt (i) == kChar) {
+                kCharCount++;
+            } else {
+                nonKCharCount++;
+            }
+        }
+
+        if (nonKCharCount >= kCharCount) {
+            nonKCharCount = 0;
+            for (int i = 0; i < k; i++) {
+                if (s.charAt (i) != kChar) {
+                    nonKCharCount++;
+                    if (kCharCount == nonKCharCount) {
+                        return i + 1;
+                    }
+                }
+            }
+        } else {
+            for (int i = k; i < s.length (); i++) {
+                if (s.charAt (i) != kChar) {
+                    nonKCharCount++;
+                }
+                if (kCharCount == nonKCharCount) {
+                    return i + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static void writeFile () throws IOException {
+        try (BufferedWriter bw = new BufferedWriter (new FileWriter (OUTPUT_TXT))) {
+            for (int i = 0; i < resultsList.size (); i++) {
+                bw.write (resultsList.get (i));
+                if (i != resultsList.size () - 1) {
                     bw.newLine ();
                 }
             }
         }
-    }
-
-    public static int calculateOutputNumber (int l, int r, int k, String s) {
-        char kChar = s.charAt (k - 1);
-        char charToCount = kChar == 'A' ? 'B' : 'A';
-
-        if (r - l + 1 < k) {
-            return -1;
-        }
-
-        if (l > r || r < 1 || l < 1) {
-            return -1;
-        }
-
-        int lookupCharCount = 0;
-        int otherCharCount = 0;
-        for (int i = 0; i < k; i++) {
-            if (s.charAt (i) == kChar) {
-                otherCharCount++;
-            }
-        }
-
-        for (int i = 0; i < s.length (); i++) {
-            if (s.charAt (i) == charToCount) {
-                lookupCharCount++;
-            }
-            if (otherCharCount == lookupCharCount) {
-                return i + 1;
-            }
-        }
-        return -1;
     }
 
 }
--- src/test/java/com/example/SolutionAppTest.java
+++ src/test/java/com/example/SolutionAppTest.java
@@ -8,7 +8,7 @@ public class SolutionAppTest extends TestCase {
 
     public void testSolution () {
        assertEquals (3, calculateOutputNumber (1, 4, 4, "ABBABAAB".substring (1 - 1, 4)));
-        assertEquals (3, calculateOutputNumber (2, 6, 1, "ABBABAAB".substring (2 - 1, 6)));
-        assertEquals (-1, calculateOutputNumber (3, 7, 5, "ABBABAAB".substring (3 - 1, 7)));
+      assertEquals (3, calculateOutputNumber (2, 6, 1, "ABBABAAB".substring (2 - 1, 6)));
+       assertEquals (-1, calculateOutputNumber (3, 7, 5, "ABBABAAB".substring (3 - 1, 7)));
     }
 }
